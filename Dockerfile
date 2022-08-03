097c4b741fada2b9238599b15d4823efec96fb74
Qi Zhang <smallqi1@163.com>
2022-08-03 10:58:41 +0800

    change dockerfile
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:3.15
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
     && apk add --no-cache wget
 
-COPY entrypoint.sh /
-VOLUME [ "/tmp" ]
+COPY entrypoint.sh /agent
+VOLUME [ "/agent" ]
 
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT [ "/agent/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 VERSION=${VERSION:-v1.16.0}
 
-if [ -f "/tmp/opentelemetry-javaagent.jar" ]; then
-  echo "File \"/tmp/opentelemetry-javaagent.jar\" exists"
+if [ -f "/agent/opentelemetry-javaagent.jar" ]; then
+  echo "File \"/agent/opentelemetry-javaagent.jar\" exists"
 else
   echo "Downloading OpenTelemetry Java Agent..."
-  wget -P /tmp https://ghproxy.com/https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/"$VERSION"/opentelemetry-javaagent.jar
+  wget -P /agent https://ghproxy.com/https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/"$VERSION"/opentelemetry-javaagent.jar
   echo "Downloaded OpenTelemetry Java Agent"
 fi
 
